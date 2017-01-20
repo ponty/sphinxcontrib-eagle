ad5078f3c9a28a64cf9a9e0bef765bf0eb48a80e
ponty <ponty@home>
2017-01-20 17:24:29 +0100

    version++

--- sphinxcontrib/eagle.py
+++ sphinxcontrib/eagle.py
@@ -12,7 +12,7 @@ import docutils.parsers.rst.directives.images
 import logging
 import os
 
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 log = logging.getLogger(__name__)
 log.debug('sphinxcontrib.eagle (version:%s)' % __version__)
